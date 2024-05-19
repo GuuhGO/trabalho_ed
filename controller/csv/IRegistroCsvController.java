4ec72a81bc1013ce19fa44a5051c7cac669d2821
Renan <109163037+rznan@users.noreply.github.com>
2024-05-19 19:18:13 -0300

    update interface RegistroCsv - remove getAllRegistros, adiciona getAllObjects, getObjectById e ObjectBuilder

--- controller/csv/IRegistroCsvController.java
+++ controller/csv/IRegistroCsvController.java
@@ -2,13 +2,16 @@ package controller.csv;
 
 import datastrucures.genericList.List;
 import model.IRegistroCsv;
+import model.Produto;
 
 import java.io.IOException;
 
-public interface IRegistroCsvController {
+public interface IRegistroCsvController<T> {
     void addNewRegistro(IRegistroCsv obj) throws IOException;
     void deleteRegistro(IRegistroCsv obj) throws IOException;
     String getRegistroById(String id) throws IOException;
-    List<String> getAllRegistros() throws IOException;
+    T getObjectById(int id) throws Exception;
+    List<T> getAllObjects() throws IOException;
+    T objectBuilder(String[] campos) throws Exception;
     void createFile() throws IOException;
 }
