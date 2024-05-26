712a52ec29152c28053864c3b434ad6147d35349
Renan <109163037+rznan@users.noreply.github.com>
2024-05-26 19:39:24 -0300

    deletar a Interface e a hashtable da tabela de Tipos
    não possuem nenhum uso no nosssos planos revisados

--- controller/hashTables/IHashTable.java
+++
@@ -1,8 +0,0 @@
-package controller.hashTables;
-
-public interface IHashTable<K, V> {
-    void put(V item) throws Exception;
-    void remove(K id) throws Exception;
-    V get(K id) throws Exception;
-    int size();
-}
